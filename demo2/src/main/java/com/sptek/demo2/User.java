d6568d84baf3b93901743c94f4df1b2ef33c07df
choi-yong-jo <54878504+choi-yong-jo@users.noreply.github.com>
2024-04-05 09:03:40 +0900

    Add files via upload

---
+++ demo2/src/main/java/com/sptek/demo2/User.java
@@ -0,0 +1,85 @@
+package com.sptek.demo2;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+
+@Entity
+@Table(name="Member")
+public class User {
+
+    @Id
+    private String id;
+
+    private String password;
+
+    private String name;
+
+    private String email;
+
+    private Date inDate;
+
+    private Date upDate;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", inDate=" + inDate +
+                ", upDate=" + upDate +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getInDate() {
+        return inDate;
+    }
+
+    public void setInDate(Date inDate) {
+        this.inDate = inDate;
+    }
+
+    public Date getUpDate() {
+        return upDate;
+    }
+
+    public void setUpDate(Date upDate) {
+        this.upDate = upDate;
+    }
+}
